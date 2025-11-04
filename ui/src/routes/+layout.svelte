c93113875ba13316b171046872daee775d78c548
bartosz121 <bmagiera121@gmail.com>
2025-11-04 19:11:34 +0100

    Add pwa setup

--- ui/src/routes/+layout.svelte
+++ ui/src/routes/+layout.svelte
@@ -1,11 +1,23 @@
 <script lang="ts">
+	import { page } from '$app/state';
 	import favicon from '$lib/assets/favicon.svg';
+	import { locales, localizeHref } from '$lib/paraglide/runtime';
+	import { pwaInfo } from 'virtual:pwa-info';
 
 	let { children } = $props();
+
+	let webManifestLink = $derived(pwaInfo ? pwaInfo.webManifest.linkTag : '');
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	{@html webManifestLink}
 </svelte:head>
+
+<div style="display:none">
+	{#each locales as locale}
+		<a href={localizeHref(page.url.pathname, { locale })}>{locale}</a>
+	{/each}
+</div>
 
 {@render children()}
--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,13 +1,34 @@
 import { paraglideVitePlugin } from '@inlang/paraglide-js';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 export default defineConfig({
 	plugins: [
 		sveltekit(),
+		SvelteKitPWA({
+			registerType: 'autoUpdate',
+			devOptions: { enabled: true },
+			injectRegister: 'auto',
+			includeAssets: ['favicon.svg', 'icon.svg'],
+			manifest: {
+				name: 'ESP8266 Web',
+				short_name: 'ESP8266 Web',
+				background_color: '#ffffff',
+				theme_color: '#000000',
+				icons: [
+					{
+						src: 'icon.svg',
+						sizes: 'any',
+						type: 'image/svg+xml'
+					}
+				]
+			}
+		}),
 		paraglideVitePlugin({
 			project: './project.inlang',
-			outdir: './src/lib/paraglide'
+			outdir: './src/lib/paraglide',
+			strategy: ['localStorage', 'preferredLanguage', 'url', 'baseLocale']
 		})
 	]
 });
