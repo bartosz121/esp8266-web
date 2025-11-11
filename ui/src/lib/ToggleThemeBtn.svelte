c2b6624f8d6553878b7e19ac5633e24e74659811
bartosz121 <bmagiera121@gmail.com>
2025-11-11 11:39:14 +0100

    Update `ToggleThemeBtn` to accept set function

--- ui/src/lib/ToggleThemeBtn.svelte
+++ ui/src/lib/ToggleThemeBtn.svelte
@@ -1,23 +1,23 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import { LocalStorage } from '$lib/storage.svelte';
+	interface Props {
+		currentTheme: 'light' | 'dark';
+		setTheme: (theme: 'light' | 'dark') => void;
+	}
 
-	const themeStorage = new LocalStorage<'light' | 'dark'>('theme');
+	let { currentTheme, setTheme }: Props = $props();
 
-	let icon = $derived(themeStorage.current === 'dark' ? '🌙' : '☀️');
+	let icon = $derived(currentTheme === 'dark' ? '🌙' : '☀️');
 
 	function toggleTheme(_: Event) {
-		const currentTheme = document.documentElement.getAttribute('data-theme');
 		const newTheme = currentTheme === 'light' ? 'dark' : 'light';
-		document.documentElement.setAttribute('data-theme', newTheme);
-		themeStorage.current = newTheme;
+		setTheme(newTheme);
 	}
 </script>
 
 <button
 	onclick={toggleTheme}
 	aria-label="Toggle color scheme"
-	class="mt-4 cursor-pointer rounded-lg border border-gray-300 bg-gray-100 px-4 py-2 text-sm text-gray-900 transition-colors hover:bg-gray-200 md:mt-0 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-100 dark:hover:bg-gray-700"
+	class="mt-4 cursor-pointer rounded-lg border border-gray-300 bg-gray-100 px-4 py-2 text-sm text-gray-900 transition-colors hover:bg-gray-200 md:mt-0 dark:border-gray-700 dark:bg-gray-950 dark:text-gray-100 dark:hover:bg-gray-800"
 >
 	{icon}
 </button>
